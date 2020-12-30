2616f0d174473d84a7e7ca36f8590745e17c5ec2
ilovric00 <ilovric@profico.hr>
2020-12-30 15:37:18 +0100

    PW-36 Headers fix

--- lib/api.ts
+++ lib/api.ts
@@ -2,18 +2,14 @@ import Draft from 'types/posts/draft';
 import Post from 'types/posts/post';
 import { GET_PREVIEW_POST, GET_ALL_POSTS_WITH_SLUG, GET_ALL_POSTS_FOR_HOME, GET_POST_AND_MORE_POSTS } from './queries';
 
-const API_URL = process.env.WORDPRESS_API_URL;
+const API_URL = `${process.env.WORDPRESS_API_URL}`;
 
 async function fetchAPI(query: string, { variables = {} } = {}) {
-  type Headers = {
-    'Content-Type': string;
-    Authorization?: string;
-  };
-
-  const headers: Headers = { 'Content-Type': 'application/json' };
+  const headers = new Headers;
+  headers.append( 'Content-Type', 'application/json' );
 
   if (process.env.WORDPRESS_AUTH_REFRESH_TOKEN) {
-    headers.Authorization = `Bearer ${process.env.WORDPRESS_AUTH_REFRESH_TOKEN}`;
+    headers.append('Authorization', `Bearer ${process.env.WORDPRESS_AUTH_REFRESH_TOKEN}`);
   }
 
   const res = await fetch(API_URL, {
