3518a3c9e8890bc9e8c710f1b25ff42554582170
Filip Polić <fpolic@profico.hr>
2021-01-08 11:04:39 +0100

    PW-26: Update contact form styles, useBreakpoint hook

--- utils/hooks/useBreakpoint.ts
+++ utils/hooks/useBreakpoint.ts
@@ -1,26 +1,21 @@
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { Theme } from '@material-ui/core/styles';
+
+import { useTheme } from '@material-ui/core/styles';
 import { BreakpointValues } from '@material-ui/core/styles/createBreakpoints';
 
-type Breakpoint = keyof BreakpointValues;
-
-const BetweenLimits = (upper: Breakpoint, lower: Breakpoint): boolean =>
-  useMediaQuery((theme: Theme) => theme.breakpoints.between(upper, lower));
-const BelowLimit = (limit: Breakpoint): boolean => useMediaQuery((theme: Theme) => theme.breakpoints.down(limit));
-const AboveLimit = (limit: Breakpoint): boolean => useMediaQuery((theme: Theme) => theme.breakpoints.up(limit));
-const OnlyLimit = (limit: Breakpoint): boolean => useMediaQuery((theme: Theme) => theme.breakpoints.only(limit));
+export type Breakpoint = keyof BreakpointValues;
 
 const useBreakpoint = () => {
-  const isBetween = (lower: Breakpoint, upper: Breakpoint) => BetweenLimits(lower, upper);
-  const isBelow = (limit: Breakpoint) => BelowLimit(limit);
-  const isAbove = (limit: Breakpoint) => AboveLimit(limit);
-  const isOnly = (limit: Breakpoint) => OnlyLimit(limit);
+  const { breakpoints } = useTheme();
+
+  const isMobile = useMediaQuery(() => breakpoints.down('sm'));
+  const isTablet = useMediaQuery(() => breakpoints.between('sm', 'lg'));
+  const isDesktop = useMediaQuery(() => breakpoints.up('lg'));
 
   return {
-    isBetween,
-    isBelow,
-    isAbove,
-    isOnly,
+    isMobile,
+    isDesktop,
+    isTablet,
   };
 };
 
