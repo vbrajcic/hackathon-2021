63016cb205b7b52387010e45009b6bbeeae9dcb6
Filip Polić <fpolic@profico.hr>
2021-01-11 10:20:23 +0100

    PW-29: Move Home component to subfolder

---
+++ pages/home/Home.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import Container from '@material-ui/core/Container';
+
+import Intro from 'components/Intro';
+import Layout from 'components/Layout';
+import Hyperlink from 'components/Hyperlink';
+import ContactForm from 'components/ContactForm';
+import links from 'config/links';
+
+const Home = () => (
+  <Layout>
+    <Head>
+      <title>Profico</title>
+    </Head>
+    <Container maxWidth="xl">
+      <Intro text="Home" />
+      {links.map(({ text, url }, index) => (
+        <Hyperlink key={url} href={url}>{`${text}${index !== links.length - 1 ? ' | ' : ''}`}</Hyperlink>
+      ))}
+      <ContactForm />
+    </Container>
+  </Layout>
+);
+
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,3 @@
-import Head from 'next/head';
-import Container from '@material-ui/core/Container';
+import Home from './home';
 
-import Intro from 'components/Intro';
-import Layout from 'components/Layout';
-import Hyperlink from 'components/Hyperlink';
-import ContactForm from 'components/ContactForm';
-import links from 'config/links';
-
-const HomePage = () => (
-  <Layout>
-    <Head>
-      <title>Profico</title>
-    </Head>
-    <Container maxWidth="xl">
-      <Intro text="Home" />
-      {links.map(({ text, url }, index) => (
-        <Hyperlink key={url} href={url}>{`${text}${index !== links.length - 1 ? ' | ' : ''}`}</Hyperlink>
-      ))}
-      <ContactForm />
-    </Container>
-  </Layout>
-);
-
-export default HomePage;
+export default Home;
