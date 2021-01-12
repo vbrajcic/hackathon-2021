7948c560eeddbcd0bf9ab1907f22d98e078113b1
Ivan Romac <iromac@profico.hr>
2021-01-12 13:30:40 +0100

    Update descriptions of expertise categories

--- config/expertiseCategories.ts
+++ config/expertiseCategories.ts
@@ -7,22 +7,22 @@ interface Category {
 const expertiseCategories: Category[] = [
   {
     name: 'Product Strategy',
-    description: 'Hollistic understanding of what the user expects, wants and hates about digital products',
+    description: 'Hollistic understanding of what the user expects, wants and hates about digital products.',
     icon: 'ProductStrategy',
   },
   {
     name: 'Design & UX',
-    description: 'Hollistic understanding of what the user expects, wants and hates about digital products',
+    description: 'Driven by research insights we frame an abstract idea into an overall product context.',
     icon: 'Design',
   },
   {
     name: 'Web Development',
-    description: 'Hollistic understanding of what the user expects, wants and hates about digital products',
+    description: 'Our development team excels at building high performance products across all platforms and devices.',
     icon: 'Web',
   },
   {
     name: 'Mobile Development',
-    description: 'Hollistic understanding of what the user expects, wants and hates about digital products',
+    description: 'Once we’re clear for take-off, what was once a user interface, now becomes a robust digital product.',
     icon: 'Mobile',
   },
 ];
