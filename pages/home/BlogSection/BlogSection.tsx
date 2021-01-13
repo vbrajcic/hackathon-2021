3a2ce14233853599729d2a477fcb6f5f0209892a
Filip Polić <fpolic@profico.hr>
2021-01-13 13:57:47 +0100

    PW-57: Merge with dev changes

--- pages/home/BlogSection/BlogSection.tsx
+++ pages/home/BlogSection/BlogSection.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Hyperlink from 'components/Hyperlink';
 
 import BlogPosts from './BlogPosts';
+import SideTitle from '../SideTitle';
 
 import styles from './BlogSection.module.scss';
 
 const BlogSection: React.FC = () => (
-  <Grid container direction="column" component="section" className={styles.container}>
-    <Typography variant="h2">
-      Find out what we’ve been
-      <br />
-      talking about
-    </Typography>
-    <Typography variant="body2" color="textSecondary" className={styles.subtitle}>
-      We shape our core capabilities around lean product teams capable of delivering immense value to organisations
-      worldwide
-    </Typography>
-    <Hyperlink href="/blog" component={<Button className={styles.button}>Go to our blog</Button>} />
-    <BlogPosts />
-  </Grid>
+  <Container maxWidth="xl" component="section" className={styles.container}>
+    <SideTitle title="Blog" />
+    <Grid className={styles.wrapper}>
+      <Typography variant="h2">
+        Find out what we’ve been
+        <br />
+        talking about
+      </Typography>
+      <Typography variant="body2" color="textSecondary" className={styles.subtitle}>
+        We shape our core capabilities around lean product teams capable of delivering immense value to organisations
+        worldwide
+      </Typography>
+      <Hyperlink href="/blog" component={<Button>Go to our blog</Button>} />
+      <BlogPosts />
+    </Grid>
+  </Container>
 );
 
 export default BlogSection;
