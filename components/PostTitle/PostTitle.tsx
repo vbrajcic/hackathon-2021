796480e0fb5c48e41c8d3e00068a9891e95cb25b
ilovric00 <ilovric@profico.hr>
2021-01-20 15:43:52 +0100

    PW-37 responsive styles for blog body

--- components/PostTitle/PostTitle.tsx
+++ components/PostTitle/PostTitle.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import Typography from '@material-ui/core/Typography';
 
+import styles from './PostTitle.module.scss';
+
 interface PostTitleProps {
   intro?: string;
   title: string;
 }
 
 const PostTitle: React.FC<PostTitleProps> = ({ intro, title }) => (
-  <Typography variant="h1">
+  <Typography variant="h1" className={styles.container}>
     {intro && (
       <Typography color="secondary" variant="h1" component="span">
         {intro}
