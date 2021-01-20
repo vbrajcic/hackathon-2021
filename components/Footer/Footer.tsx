df96e38e5068686e0844b18491f03c391b26b6dc
Filip Polić <fpolic@profico.hr>
2021-01-20 14:57:47 +0100

    PW-70: Fix footer structure, container and sections paddings/margins

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Instagram from '@material-ui/icons/Instagram';
 import LinkedIn from '@material-ui/icons/LinkedIn';
 import SocialIcon from 'components/SocialIcon';
 import Dribble from 'components/SvgIcons/Dribble';
 import Behance from 'components/SvgIcons/Behance';
-import useBreakpoint from 'utils/hooks/useBreakpoint';
 
 import { format } from 'date-fns';
 
@@ -15,27 +14,21 @@ import styles from './Footer.module.scss';
 
 const currentYear = format(new Date(), 'yyyy');
 
-const Footer: React.FC<{}> = () => {
-  const { isMobile } = useBreakpoint();
-
-  return (
-    <footer className={styles.container}>
-      <Container>
-        <Grid container justify="space-between" alignItems="center" direction={isMobile ? 'column-reverse' : 'row'}>
-          <Typography variant="body2">
-            &copy;
-            {` ${currentYear} Profico`}
-          </Typography>
-          <Grid container classes={{ root: styles.socialIcons }}>
-            <SocialIcon icon={Instagram} url="https://www.instagram.com/profi.co/" />
-            <SocialIcon icon={Dribble} url="https://dribbble.com/Profico" />
-            <SocialIcon icon={LinkedIn} url="https://www.linkedin.com/company/profico" />
-            <SocialIcon icon={Behance} url="https://www.behance.net/profico" />
-          </Grid>
-        </Grid>
-      </Container>
-    </footer>
-  );
-};
+const Footer: React.FC = () => (
+  <Container component="footer" maxWidth="xl" className={styles.container}>
+    <Container maxWidth={false} className={styles.wrapper}>
+      <Typography variant="body2">
+        &copy;
+        {` ${currentYear} Profico`}
+      </Typography>
+      <Grid container classes={{ root: styles.socialIcons }}>
+        <SocialIcon icon={Instagram} url="https://www.instagram.com/profi.co/" />
+        <SocialIcon icon={Dribble} url="https://dribbble.com/Profico" />
+        <SocialIcon icon={LinkedIn} url="https://www.linkedin.com/company/profico" />
+        <SocialIcon icon={Behance} url="https://www.behance.net/profico" />
+      </Grid>
+    </Container>
+  </Container>
+);
 
 export default Footer;
