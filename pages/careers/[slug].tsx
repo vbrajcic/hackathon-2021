b797964ca8fedb4377d1decc641ecebec65de477
Filip Polić <fpolic@profico.hr>
2021-01-25 14:53:03 +0100

    PW-72: Update relative paths to 'views' folder to be absolute

--- pages/careers/[slug].tsx
+++ pages/careers/[slug].tsx
@@ -7,13 +7,12 @@ import PostHeader from 'components/PostHeader';
 import Layout from 'components/Layout';
 import PostTitle from 'components/PostTitle';
 import ApplyForm from 'views/careers/ApplyForm';
+import EasterEggPostButton from 'views/careers/EasterEggPostButton';
 
 import { getAllPostsWithSlug, getPostAndMorePosts } from 'lib/api';
 import { GetPostAndMorePostsResult } from 'lib/queries';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import { useRouter } from 'next/router';
-
-import EasterEggPostButton from '../../views/careers/EasterEggPostButton';
 
 import styles from './Careers.module.scss';
 
--- pages/careers/index.tsx
+++ pages/careers/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Post from 'types/posts/post';
 import Layout from 'components/Layout';
+import WhoWeAreSection from 'views/careers/WhoWeAreSection';
+import OpenPositions from 'views/careers/OpenPositions';
 
 import { GetServerSideProps } from 'next';
 import { Edges } from 'types/common';
 import { getAllCareerPosts } from 'lib/api';
-
-import WhoWeAreSection from '../../views/careers/WhoWeAreSection';
-import OpenPositions from '../../views/careers/OpenPositions';
 
 interface CareersPageProps {
   posts: Edges<Post>;
