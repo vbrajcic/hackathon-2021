ba43ef9d121f7041f5e558e173d59d77c5f52cef
ilovric00 <ilovric@profico.hr>
2021-01-27 13:55:47 +0100

    PW-76 fix for featured post

--- pages/blog/ReadAboutUs/ReadAboutUs.tsx
+++ pages/blog/ReadAboutUs/ReadAboutUs.tsx
@@ -14,22 +14,24 @@ const ReadAboutUs: React.FC = () => {
   } = useBlogPosts();
 
   const topPosts = posts.map(({ node }) => node).slice(0, 3);
-  const featuredPost = posts[3].node;
+  const featuredPost = posts[3]?.node;
   const bottomPosts = posts.map(({ node }) => node).slice(4, 7);
 
   return (
     <Container maxWidth="xl" component="section" className={styles.container} disableGutters>
       <Typography variant="h2">Read about us</Typography>
       <BlogPosts posts={topPosts} />
-      <Grid className={styles.teaser}>
-        <Teaser
-          title={featuredPost.title}
-          excerpt={featuredPost.excerpt}
-          coverImage={featuredPost.featuredImage?.node}
-          url={`/blog/${featuredPost.slug}`}
-          direction="row"
-        />
-      </Grid>
+      {featuredPost && (
+        <Grid className={styles.teaser}>
+          <Teaser
+            title={featuredPost.title}
+            excerpt={featuredPost.excerpt}
+            coverImage={featuredPost.featuredImage?.node}
+            url={`/blog/${featuredPost.slug}`}
+            direction="row"
+          />
+        </Grid>
+      )}
       <BlogPosts posts={bottomPosts} />
     </Container>
   );
