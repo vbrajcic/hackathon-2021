8ca1e1e49e066f3afb822c4d86423a35087545ef
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2021-02-09 11:44:50 +0100

    PW-83 Implement MVP section

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,10 +1,12 @@
 import Layout from 'components/Layout';
 import LeadershipSection from 'views/about/LeadershipSection';
+import MVPSection from 'views/about/MVPSection';
 import OurRules from 'views/about/OurRules';
 
 const AboutPage = () => (
   <Layout title="About us">
     <LeadershipSection />
+    <MVPSection />
     <OurRules />
   </Layout>
 );
