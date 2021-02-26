8355efc996027b11003e2e651e0a84033467aa1b
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2021-02-26 15:27:08 +0100

    PW-100: Fix testimonial cards height

--- views/home/ReferencesSection/QuoteCards/QuoteCard/QuoteCard.tsx
+++ views/home/ReferencesSection/QuoteCards/QuoteCard/QuoteCard.tsx
@@ -4,6 +4,7 @@ import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
 import useBreakpoint from 'utils/hooks/useBreakpoint';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
 
 import { Quote } from 'config/quotes';
 
@@ -18,31 +19,33 @@ const QuoteCard: React.FC<QuoteProps> = ({ quote: { text, companyImgSrc, author 
 
   return (
     <Card className={styles.container} classes={{ root: styles.root }}>
-      <Grid container className={styles.textWrapper}>
-        <FormatQuoteIcon className={styles.quotesIcon} />
-        <Typography variant="body1" className={styles.quoteText}>
-          {text}
-        </Typography>
-      </Grid>
-      <Grid container alignItems="center" justify="space-between" className={styles.footer}>
-        <Grid container alignItems="center">
-          <img src={companyImgSrc} alt="Partner's logo" className={styles.companyImg} />
-          <Grid container direction="column" className={styles.authorNameAndTitleWrapper}>
-            <Typography variant="body2" className={styles.authorName}>
-              {author.name}
-            </Typography>
-            <Typography variant="body2">{author.title}</Typography>
-          </Grid>
+      <CardContent className={styles.content}>
+        <Grid container className={styles.textWrapper}>
+          <FormatQuoteIcon className={styles.quotesIcon} />
+          <Typography variant="body1" className={styles.quoteText}>
+            {text}
+          </Typography>
         </Grid>
-        {!isMobile && (
-          <div
-            style={{
-              backgroundImage: `url(${author.imgSrc})`,
-            }}
-            className={styles.authorImg}
-          />
-        )}
-      </Grid>
+        <Grid container alignItems="center" justify="space-between" className={styles.footer}>
+          <Grid container alignItems="center">
+            <img src={companyImgSrc} alt="Partner's logo" className={styles.companyImg} />
+            <Grid container direction="column" className={styles.authorNameAndTitleWrapper}>
+              <Typography variant="body2" className={styles.authorName}>
+                {author.name}
+              </Typography>
+              <Typography variant="body2">{author.title}</Typography>
+            </Grid>
+          </Grid>
+          {!isMobile && (
+            <div
+              style={{
+                backgroundImage: `url(${author.imgSrc})`,
+              }}
+              className={styles.authorImg}
+            />
+          )}
+        </Grid>
+      </CardContent>
     </Card>
   );
 };
