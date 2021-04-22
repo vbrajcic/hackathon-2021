cf0e8dc8831434126cf8bc7e886608f7c5b7f642
Vana Brajčić <vbrajcic@profico.hr>
2021-04-22 13:51:03 +0200

    PW-147 favicon update, tracking setup test

--- components/Meta/Meta.tsx
+++ components/Meta/Meta.tsx
@@ -6,16 +6,38 @@ const Meta = () => (
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
     <link rel="manifest" href="/favicon/site.webmanifest" />
-    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#ffffff" />
     <link rel="shortcut icon" href="/favicon/favicon.ico" />
     <meta name="msapplication-TileColor" content="#000000" />
     <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
     <meta name="theme-color" content="#000" />
     <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-    <meta
-      name="description"
-      content="Profico - professional mobile, web and social networking applications development, implying dedicated and responsible approach to every task and client."
-    />
+
+    <link href="/favicon/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+    <link href="/favicon/favicon-230x230.png" rel="icon" sizes="230x230" type="image/png" />
+    <link href="/favicon/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+    <link href="/favicon/favicon-96x96.png" rel="icon" sizes="96x96" type="image/png" />
+    <link href="/favicon/favicon.ico" rel="shortcut icon" />
+
+    <link href="/favicon/android-icon-36x36.png" rel="icon" sizes="36x36" type="image/png" />
+    <link href="/favicon/android-icon-48x48.png" rel="icon" sizes="48x48" type="image/png" />
+    <link href="/favicon/android-icon-72x72.png" rel="icon" sizes="72x72" type="image/png" />
+    <link href="/favicon/android-icon-96x96.png" rel="icon" sizes="96x96" type="image/png" />
+    <link href="/favicon/android-icon-144x144.png" rel="icon" sizes="144x144" type="image/png" />
+    <link href="/favicon/android-icon-192x192.png" rel="icon" sizes="192x192" type="image/png" />
+
+    <link href="/favicon/manifest.json" rel="manifest" />
+    <link href="/favicon/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114" />
+    <link href="/favicon/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120" />
+    <link href="/favicon/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144" />
+    <link href="/favicon/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
+    <link href="/favicon/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180" />
+    <link href="/favicon/apple-touch-icon-57x57.png" rel="apple-touch-icon" sizes="57x57" />
+    <link href="/favicon/apple-touch-icon-60x60.png" rel="apple-touch-icon" sizes="60x60" />
+    <link href="/favicon/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72" />
+    <link href="/favicon/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76" />
+
+    <meta name="description" content="Profico - Mobile app and web development agency." />
     <meta property="og:image" content="/logo/logo-sharing.png" />
   </Head>
 );
