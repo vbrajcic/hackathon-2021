646074f6a69d5ea4b0bab2957cc4758c3e2d5e01
Vana Brajčić <vbrajcic@profico.hr>
2021-04-22 14:24:07 +0200

    PW-147 links to array

--- components/Meta/Meta.tsx
+++ components/Meta/Meta.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import { metaIcons } from 'config/meta';
+
 const Meta = () => (
   <Head>
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
@@ -13,29 +15,24 @@ const Meta = () => (
     <meta name="theme-color" content="#000" />
     <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
 
-    <link href="/favicon/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-    <link href="/favicon/favicon-230x230.png" rel="icon" sizes="230x230" type="image/png" />
-    <link href="/favicon/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
-    <link href="/favicon/favicon-96x96.png" rel="icon" sizes="96x96" type="image/png" />
+    {metaIcons.favicons.map(size => (
+      <link href={`/favicon/favicon-${size}.png`} rel="icon" key={size} sizes={size} type="image/png" />
+    ))}
+    {metaIcons.android.map(size => (
+      <link href={`/favicon/android-icon-${size}.png`} rel="icon" key={size} sizes={size} type="image/png" />
+    ))}
+    {metaIcons.apple.map(size => (
+      <link
+        href={`/favicon/apple-touch-icon-${size}.png`}
+        rel="apple-touch-icon"
+        key={size}
+        sizes={size}
+        type="image/png"
+      />
+    ))}
+
     <link href="/favicon/favicon.ico" rel="shortcut icon" />
-
-    <link href="/favicon/android-icon-36x36.png" rel="icon" sizes="36x36" type="image/png" />
-    <link href="/favicon/android-icon-48x48.png" rel="icon" sizes="48x48" type="image/png" />
-    <link href="/favicon/android-icon-72x72.png" rel="icon" sizes="72x72" type="image/png" />
-    <link href="/favicon/android-icon-96x96.png" rel="icon" sizes="96x96" type="image/png" />
-    <link href="/favicon/android-icon-144x144.png" rel="icon" sizes="144x144" type="image/png" />
-    <link href="/favicon/android-icon-192x192.png" rel="icon" sizes="192x192" type="image/png" />
-
     <link href="/favicon/manifest.json" rel="manifest" />
-    <link href="/favicon/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114" />
-    <link href="/favicon/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120" />
-    <link href="/favicon/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144" />
-    <link href="/favicon/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
-    <link href="/favicon/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180" />
-    <link href="/favicon/apple-touch-icon-57x57.png" rel="apple-touch-icon" sizes="57x57" />
-    <link href="/favicon/apple-touch-icon-60x60.png" rel="apple-touch-icon" sizes="60x60" />
-    <link href="/favicon/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72" />
-    <link href="/favicon/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76" />
 
     <meta name="description" content="Profico - Mobile app and web development agency." />
     <meta property="og:image" content="/logo/logo-sharing.png" />
