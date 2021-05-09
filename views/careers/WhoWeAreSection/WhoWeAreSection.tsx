3209df98b586206764d2b45929ae74b8c2f3e12d
Vana Brajčić <vbrajcic@profico.hr>
2021-05-09 20:25:50 +0200

    PW-162 updated images on careers page

--- views/careers/WhoWeAreSection/WhoWeAreSection.tsx
+++ views/careers/WhoWeAreSection/WhoWeAreSection.tsx
@@ -17,17 +17,19 @@ const WhoWeAreSection: FC = () => (
         busy since 2012
       </Typography>
       <Typography variant="body2" className={styles.subtitle}>
-        With offices in two of the largest cities in Croatia (Zagreb & Split) and openness to the remote positions,
-        we’re always looking for talented people to join our team! If you are in love with what you do, that makes you
-        one of us. This is our invitation to join us!
+        With offices in two of the largest cities in Croatia (Zagreb & Split) and openness to remote positions, we’re
+        always looking for talented people to join our team! If you are in love with what you do, that makes you one of
+        us. This is our invitation to join us!
       </Typography>
     </Grid>
     <Grid className={styles.imageWrapper}>
       <Grid className={styles.innerImageWrapper}>
-        <img src={addAssetPrefix('images/careers/who-we-are-1.jpg')} className={styles.image} alt="Who we are 01>" />
-        <img src={addAssetPrefix('images/careers/who-we-are-2.jpg')} className={styles.image} alt="Who we are 02>" />
-        <img src={addAssetPrefix('images/careers/who-we-are-3.jpg')} className={styles.image} alt="Who we are 03>" />
-        <img src={addAssetPrefix('images/careers/who-we-are-4.jpg')} className={styles.image} alt="Who we are 04>" />
+        <img src={addAssetPrefix('images/careers/who_we_are-1.jpg')} className={styles.image} alt="Who we are 01" />
+        <img src={addAssetPrefix('images/careers/who_we_are-2.jpg')} className={styles.image} alt="Who we are 02" />
+        <img src={addAssetPrefix('images/careers/who_we_are-3.jpg')} className={styles.image} alt="Who we are 03" />
+        <img src={addAssetPrefix('images/careers/who_we_are-4.jpg')} className={styles.image} alt="Who we are 04" />
+        <img src={addAssetPrefix('images/careers/who_we_are-5.jpg')} className={styles.image} alt="Who we are 05" />
+        <img src={addAssetPrefix('images/careers/who_we_are-6.jpg')} className={styles.image} alt="Who we are 06" />
       </Grid>
     </Grid>
   </Container>
