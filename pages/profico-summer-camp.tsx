e77fed2a83db588ca425f6d456e5b3f72753f071
ilovric00 <ilovric@profico.hr>
2021-05-14 11:10:25 +0200

    PW-171 initial draft for PSC 2021

---
+++ pages/profico-summer-camp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from 'components/Layout';
+import HeroSection from 'views/profico-summer-camp/HeroSection';
+import AboutSummerCamp from 'views/profico-summer-camp/AboutSummerCamp';
+
+const ProficoSummerCamp: React.FC = () => (
+  <Layout title="Summer Camp">
+    <HeroSection />
+    <AboutSummerCamp />
+  </Layout>
+);
+
+export default ProficoSummerCamp;
