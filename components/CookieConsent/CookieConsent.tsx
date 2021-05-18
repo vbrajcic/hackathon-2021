d23db7feb242ba20c4344875d0d72e6231b4bbd4
Filip Polić <fpolic@profico.hr>
2021-05-18 10:43:55 +0200

    Remove react-cookie-consent and use react-use for cookie handling

--- components/CookieConsent/CookieConsent.tsx
+++ components/CookieConsent/CookieConsent.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
-import CookieConsent from 'react-cookie-consent';
+import cx from 'classnames';
+
+import { Box, Button, Paper, Typography } from '@material-ui/core';
+import { useCookie } from 'react-use';
 
 import styles from './CookieConsent.module.scss';
 
-const CustomCookieConsent: React.FC = () => (
-  <div className={styles.root}>
-    <CookieConsent
-      disableStyles
-      disableButtonStyles
-      buttonText="Ok"
-      cookieName="profico-web-cookie-consent"
-      containerClasses={styles.container}
-      buttonClasses={styles.button}
-      expires={365}
-    >
-      This website uses cookies in order to provide a better user experience and functionality. By continuing to browse
-      the site you agree to our <a href="/cookie-policy">Cookie</a> and <a href="/privacy-policy">Privacy Policy</a>.
-    </CookieConsent>
-  </div>
-);
+const CookieConsent: React.FC = () => {
+  const [cookie, setCookie] = useCookie('profico-web-cookie-consent');
 
-export default CustomCookieConsent;
+  if (cookie === 'true') {
+    return null;
+  }
+
+  const submitCookieConsent = () => {
+    setCookie('true', {
+      expires: 365,
+      sameSite: 'Lax',
+      secure: true,
+    });
+  };
+
+  return (
+    <Paper className={cx(styles.root, { [styles.visible]: cookie !== 'true' })} elevation={0} square>
+      <Box className={styles.container}>
+        <Typography className={styles.text} variant="body2" color="textPrimary">
+          This website uses cookies in order to provide a better user experience and functionality. By continuing to
+          browse the site you agree to our &nbsp;
+          <a href="/cookie-policy">Cookie</a>
+          &nbsp;and&nbsp;
+          <a href="/privacy-policy">Privacy Policy</a>
+          &nbsp;.
+        </Typography>
+        <Button onClick={submitCookieConsent} className={styles.button}>
+          Ok
+        </Button>
+      </Box>
+    </Paper>
+  );
+};
+
+export default CookieConsent;
