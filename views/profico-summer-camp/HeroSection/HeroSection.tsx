589da9bb1bd8a8eecd69ebed71277fcf017b169e
ilovric00 <ilovric@profico.hr>
2021-05-27 14:21:44 +0200

    PW-172 Implement Hero section

--- views/profico-summer-camp/HeroSection/HeroSection.tsx
+++ views/profico-summer-camp/HeroSection/HeroSection.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import Intro from 'components/Intro';
-import Button from '@material-ui/core/Button';
 import Image from 'next/image';
-import Hyperlink from 'components/Hyperlink';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 
 import { addAssetPrefix } from 'utils/static/addAssetPrefix';
+
+import Intro from './Intro';
 
 import styles from './HeroSection.module.scss';
 
 const HeroSection: React.FC = () => (
   <Grid component="section" className={styles.root}>
     <Container className={styles.container} maxWidth="xl">
-      <Intro text="Ready to build apps?" className={styles.intro}>
-        <Hyperlink
-          data-ga-event-name="go_to_services"
-          href="#"
-          component={
-            <Button className={styles.button} color="secondary">
-              Hell Yeah! Let&apos;s go
-            </Button>
-          }
-        />
-      </Intro>
+      <Intro />
       <Grid className={styles.imageWrapper}>
         <Image
           src={addAssetPrefix('/images/summerCamp/psc-hero.png')}
