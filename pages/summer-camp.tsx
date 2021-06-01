0f6d31b9276052cf3b4ce7a1251a5a5105710250
Filip Polić <fpolic@profico.hr>
2021-06-01 14:09:55 +0200

    PW-176: Add last year section (desktop only)

--- pages/summer-camp.tsx
+++ pages/summer-camp.tsx
@@ -6,6 +6,7 @@ import SummerCampPerks from 'views/summer-camp/SummerCampPerks';
 import AboutMentors from 'views/summer-camp/AboutMentors';
 import SummerCampFocusPoints from 'views/summer-camp/SummerCampFocusPoints';
 import IdealCandidate from 'views/summer-camp/IdealCandidate';
+import LastYearSection from 'views/summer-camp/LastYearSection';
 
 const ProficoSummerCamp: React.FC = () => (
   <Layout title="Summer Camp" theme="dark">
@@ -14,6 +15,7 @@ const ProficoSummerCamp: React.FC = () => (
     <SummerCampPerks />
     <SummerCampFocusPoints />
     <AboutMentors />
+    <LastYearSection />
     <IdealCandidate />
   </Layout>
 );
--- views/summer-camp/SummerCampPerks/PerkCard/PerkCard.tsx
+++ views/summer-camp/SummerCampPerks/PerkCard/PerkCard.tsx
@@ -9,7 +9,7 @@ import styles from './PerkCard.module.scss';
 const PerkCard: React.FC<SummerCampPerk> = ({ title, subtitle, icon: Icon }) => (
   <Card elevation={0} classes={{ root: styles.root }} className={styles.container}>
     <Icon className={styles.icon} />
-    <Typography variant="h4" component="h3" className={styles.title}>
+    <Typography variant="h5" component="h3" className={styles.title}>
       {title}
     </Typography>
     <Typography variant="body2" className={styles.subtitle}>
