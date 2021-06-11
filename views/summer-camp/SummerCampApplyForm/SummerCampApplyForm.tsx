f61cba070ff82316d66c8531e619822c6fb0a04b
ikraljevic-profico <ikraljevic@profico.hr>
2021-06-11 10:44:11 +0200

    PW-209: add line

--- views/summer-camp/SummerCampApplyForm/SummerCampApplyForm.tsx
+++ views/summer-camp/SummerCampApplyForm/SummerCampApplyForm.tsx
@@ -105,6 +105,7 @@ const SummerCampApplyForm: React.FC = () => {
 
               const cvFilename = typeof cv !== 'string' ? cv?.name : '';
               const motivationFilename = typeof motivationLetter !== 'string' ? motivationLetter?.name : '';
+
               return (
                 <>
                   <FormInput
