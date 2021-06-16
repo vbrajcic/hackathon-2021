672319fc83a25655db33655a46463a0a012fea86
ikraljevic-profico <ikraljevic@profico.hr>
2021-06-16 15:40:06 +0200

    PW-213: updated icons on the Careers Hero Section

--- views/careers/HeroSection/HeroCard/HeroCard.tsx
+++ views/careers/HeroSection/HeroCard/HeroCard.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import FingerTouch from 'components/SvgIcons/Fingertouch';
 import cn from 'clsx';
+import { CareerHeroCard } from 'config/careersHeroCards';
+
 import styles from './HeroCard.module.scss';
 
-interface HeroCardProps {
-  color: string;
-  text: string;
-  isSpecial?: boolean;
-}
-
-const HeroCard: React.FC<HeroCardProps> = ({ color, text, isSpecial }) => (
+const HeroCard: React.FC<CareerHeroCard> = ({ icon: Icon, title, isSpecial }) => (
   <div className={cn(styles.card, { [styles.cardSpecial]: isSpecial })}>
     <div className={styles.image}>
-      <FingerTouch fill={color} className={styles.cardIcon} />
+      <Icon className={styles.cardIcon} />
     </div>
     <Typography className={styles.text} variant="h5" component="h2">
-      {text}
+      {title}
     </Typography>
   </div>
 );
