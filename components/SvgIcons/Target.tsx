30a3834e6695d54d9104a99237255299c6c3b81c
ikraljevic-profico <ikraljevic@profico.hr>
2021-06-16 10:09:42 +0200

    PW-212: updated icons on Our core values

---
+++ components/SvgIcons/Target.tsx
@@ -0,0 +1,52 @@
+import React, { FC, SVGProps } from 'react';
+
+const Target: FC<SVGProps<SVGSVGElement>> = props => (
+  <svg width="47" height="47" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M21.5728 23.2239L29.7331 15.0635"
+      stroke="white"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M29.7334 15.0635L30.8457 7.71975L37.2064 1.46875L38.1895 6.60742L43.3282 7.5905L37.0771 13.9512L29.7334 15.0635V15.0635Z"
+      stroke="white"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M41.2327 18.6295C43.3629 26.7046 39.998 35.2271 32.9258 39.669C25.8537 44.1108 16.7155 43.4411 10.3664 38.0157C4.01739 32.5903 1.93093 23.6683 5.21563 15.99C8.50032 8.31175 16.3939 3.6591 24.7024 4.50407"
+      stroke="white"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M32.7471 21.0266C33.8031 25.2847 32.0414 29.7439 28.3602 32.1305C24.679 34.5171 19.8892 34.3054 16.4329 31.6034C12.9766 28.9013 11.6151 24.3041 13.0427 20.1558C14.4703 16.0074 18.3725 13.2216 22.7596 13.2188"
+      stroke="white"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12.7603 39.6562L11.2915 45.5313"
+      stroke="white"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M33.3228 39.6562L34.7915 45.5313"
+      stroke="white"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default Target;
--- config/coreValues.ts
+++ config/coreValues.ts
@@ -1,4 +1,4 @@
-export type CoreValueIcon = 'REACT' | 'TEST_TUBE' | 'ALIEN' | 'ROCKET';
+export type CoreValueIcon = 'STAR' | 'CUSTOMER' | 'TARGET' | 'EMPATHY';
 export interface CoreValue {
   icon: CoreValueIcon;
   title: string;
@@ -7,24 +7,24 @@ export interface CoreValue {
 
 const coreValues: CoreValue[] = [
   {
-    icon: 'REACT',
+    icon: 'TARGET',
     title: 'Goal oriented',
     subtitle:
       'Achieving goals is the single most important objective behind everything we do. Measure success. Learn. Improve.',
   },
   {
-    icon: 'TEST_TUBE',
+    icon: 'CUSTOMER',
     title: 'Customer-centric',
     subtitle:
       'Customers are the center of Profico’s universe. On our mission to solve your problems, we always take that extra mile!',
   },
   {
-    icon: 'ALIEN',
+    icon: 'EMPATHY',
     title: 'User empathy',
     subtitle: 'We always put ourselves in the perspective of the end user to understand what makes his life easier!',
   },
   {
-    icon: 'ROCKET',
+    icon: 'STAR',
     title: 'Always deliver a little more!',
     subtitle: 'We go beyond delivering what just works. We always take that extra mile!',
   },
