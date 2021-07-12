512d38d71850ba295de6f9d233fa5b2ba7d103a6
ikraljevic-profico <ikraljevic@profico.hr>
2021-07-12 16:03:24 +0200

    PW-215: Single Blog post header updated

--- components/PostHeader/PostHeader.tsx
+++ components/PostHeader/PostHeader.tsx
@@ -2,24 +2,26 @@ import React from 'react';
 import cn from 'clsx';
 
 import Grid from '@material-ui/core/Grid';
-
-import PostExcerpt from 'components/PostExcerpt';
+import Box from '@material-ui/core/Box';
+import Author from 'types/posts/author';
 import PostTitle from 'components/PostTitle';
-import { Node } from 'types/common';
 import FeaturedImage from 'types/posts/featuredImage';
+import { Node } from 'types/common';
 import { useTheme } from 'utils/context/ThemeContext';
 
+import PostInfo from './PostInfo';
 import styles from './PostHeader.module.scss';
 
 interface PostHeaderProps {
   intro?: string;
   title: string;
-  excerpt: string;
+  author: Author;
+  content: string | undefined;
   featuredImage?: Node<FeaturedImage>;
   template?: 'static' | undefined;
 }
 
-const PostHeader: React.FC<PostHeaderProps> = ({ intro, title, excerpt, featuredImage, template }) => {
+const PostHeader: React.FC<PostHeaderProps> = ({ intro, title, author, content, featuredImage, template }) => {
   const { theme } = useTheme();
 
   return (
@@ -28,12 +30,14 @@ const PostHeader: React.FC<PostHeaderProps> = ({ intro, title, excerpt, featured
       justify="center"
       alignItems="center"
       className={cn(styles.container, { [styles.darkTheme]: theme === 'dark', [styles.static]: template === 'static' })}
-      style={{ backgroundImage: `url(${featuredImage?.node?.sourceUrl})` }}
     >
       <Grid item className={styles.wrapper}>
         <PostTitle intro={intro} title={title} />
-        <PostExcerpt excerpt={excerpt} />
+        <PostInfo author={author} content={content} />
       </Grid>
+      <Box className={styles.imageWrapper}>
+        <img className={styles.image} src={featuredImage?.node?.sourceUrl} alt="blogImage" />
+      </Box>
     </Grid>
   );
 };
---
+++ utils/static/estimateReadingTime.ts
@@ -0,0 +1,6 @@
+export const estimateReadingTime = (readingText: string) => {
+  const wordsPerMinute = 150;
+  const textWords = readingText.trim().split(/\s+/).length;
+  const estReadingTIme = Math.ceil(textWords / wordsPerMinute);
+  return estReadingTIme;
+};
