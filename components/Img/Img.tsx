646c3be067f5c5405f323f8cd87ed4aea11fa3d5
Vana Brajčić <vbrajcic@profico.hr>
2021-11-03 10:16:18 +0100

    PW-246 destruct quality prop

--- components/Img/Img.tsx
+++ components/Img/Img.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image, { ImageProps } from 'next/image';
 
-const Img: React.FC<ImageProps> = props => <Image {...props} quality={100} />;
+const Img: React.FC<ImageProps> = ({ quality = 100, ...props }) => <Image {...props} quality={quality} />;
 
 export default Img;
